fbfcb3cee63f97b495e5b2949b55c6e33cdc7daf
KhuntPratik <pratikkhunt43@gmail.com>
2026-03-18 12:42:10 +0530

    change lib file

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,10 +1,23 @@
-// lib/prisma.ts
+// // lib/prisma.ts
+// import { PrismaClient } from "@prisma/client";
+
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+
+// // Avoid multiple instances in development
+// export const prisma = globalThis.prisma || new PrismaClient();
+// if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+
+
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
 
-// Avoid multiple instances in development
-export const prisma = globalThis.prisma || new PrismaClient();
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production")
+  globalForPrisma.prisma = prisma;
